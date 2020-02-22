bda2e01e407dbc7141e862a2904326ae1e159990
david-brewu <43455749+david-brewu@users.noreply.github.com>
2020-02-22 23:09:46 +0000

    Add files via upload

---
+++ dart1.dart
@@ -0,0 +1,26 @@
+
+
+void main(){
+  int x = 10;
+  double y = 20.0;
+  String s = "${x+y}";
+  print (s);
+
+  List l = [];
+  l.add(1);
+  
+  l.add(5);
+  l.add(-4);
+  print(l);
+  l.sort();
+  print("my l is: " );
+
+  List<String> ls = [];
+  ls.add("David");
+  ls.add("Akwasi");
+  print(ls); ls.sort(); print(ls);
+ 
+ 
+
+  
+}
